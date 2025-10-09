459ee906e67288d70d6c81c4ceea78f6aa6c1ea6
Nicholas Pucci <npucci@int4.local.snellius.surf.nl>
2025-10-09 09:54:35 +0200

    Update to version 1.0.2 - fix mapping file packaging

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="bifidoannotator",
-    version="1.0.1",
+    version="1.0.2",
     author="Nicholas Pucci & Daniel R. Mende",
     author_email="n.pucci@amsterdamumc.nl",
     description="Fine-grained annotation of bifidobacterial enzymes for human milk glycan utilization",
